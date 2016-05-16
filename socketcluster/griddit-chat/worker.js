4fdf561ee9ad68a329d682e313dda1a03383ddec
Quan Yang <quanyang@live.com>
2016-05-17 01:15:20 +0200

    Working on socketcluster chat, seems promising. Basic chat works, got to work on auth

--- socketcluster/griddit-chat/worker.js
+++ socketcluster/griddit-chat/worker.js
@@ -5,40 +5,30 @@ var path = require('path');
 
 module.exports.run = function (worker) {
   console.log('   >> Worker PID:', process.pid);
-
   var app = require('express')();
-
   var httpServer = worker.httpServer;
   var scServer = worker.scServer;
-
   app.use(serveStatic(path.resolve(__dirname, 'public')));
-
   httpServer.on('request', app);
-
   var count = 0;
 
   /*
     In here we handle our incoming realtime connections and listen for events.
-  */
-  scServer.on('connection', function (socket) {
-
-    // Some sample logic to show how to handle client events,
-    // replace this with your own logic
-
-    socket.on('sampleClientEvent', function (data) {
-      count++;
-      console.log('Handled sampleClientEvent', data);
-      scServer.exchange.publish('sample', count);
-    });
-
-    var interval = setInterval(function () {
-      socket.emit('rand', {
-        rand: Math.floor(Math.random() * 5)
+    */
+    scServer.on('connection', function (socket) {  
+      console.log(socket.id);
+      console.log('User connected');
+      socket.on('info', function (data) {
+        scServer.global.publish(data.channel, {type: "info", msg:data.msg});
       });
-    }, 1000);
-
-    socket.on('disconnect', function () {
-      clearInterval(interval);
+      socket.on('chat', function (data) {
+        var time = new Date();
+        scServer.global.publish(data.channel, data);
+        console.log('Chat:', data,time.getTime());
+      });
+      socket.on('disconnect', function () {
+        console.log('User disconnected');
+      });
     });
-  });
-};
+
+  };
