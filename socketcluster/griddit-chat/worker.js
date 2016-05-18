8f94fda7e61bb1a57f9da9a60863383bc81f540d
Quan Yang <quanyang@live.com>
2016-05-18 23:45:43 +0200

    Able to assign random username, and send connect/disconnect messages

--- socketcluster/griddit-chat/worker.js
+++ socketcluster/griddit-chat/worker.js
@@ -5,6 +5,18 @@ var path = require('path');
 
 var connectMsg = "Info: %s has joined the chatroom.";
 var disconnectMsg = "Info: %s has left the chatroom.";
+var guestUsername = "Guest_%s";
+var colors = ["d-re","l-bl","mage","red","pink","blue","teal","oran","d-pu"];
+
+function generateRandomColor() {
+    var color = colors[Math.floor(Math.random() * colors.length)];
+    return color;
+}
+
+function generateGuestId() {
+  var randomInt = Math.floor((Math.random() * 10000) + 1000);
+  return guestUsername.replace("%s",randomInt);
+}
 
 module.exports.run = function (worker) {
   console.log('   >> Worker PID:', process.pid);
@@ -15,39 +27,40 @@ module.exports.run = function (worker) {
   httpServer.on('request', app);
 
   scServer.on('connection', function (socket) {  
-    console.log('User connected');
-    //console.log(socket.id);
     var authToken = socket.getAuthToken();
-    socket.on('auth', function(data,res) {
-      //Validate user
-      // Check data.username
-      socket.setAuthToken({username: data.username});
-    });
 
     //Blocks publish except for server.
     scServer.addMiddleware(scServer.MIDDLEWARE_PUBLISH_IN, function (req, next) {
       next(true);
     });
 
-    socket.on('connected', function (data) {
-      if (authToken){
-        scServer.global.publish(data.channel, {type: "info", msg: connectMsg.replace('%s',authToken.username)});
-      }
+    socket.on('auth', function(data,res) {
+      //Validate user
+      // Check data.username
+      socket.setAuthToken({username: generateGuestId(), color: generateRandomColor()});
     });
 
     socket.on('chat', function (data) {
       var time = new Date();
       data.username = authToken.username;
+      data.color = authToken.color;
       data.time = time.getTime();
       data.type = "message";
       scServer.global.publish(data.channel, data);
-      console.log('Chat:', data);
     });
 
-    socket.on('disconnect', function () {
-      console.log('User disconnected');
-      //scServer.global.publish(data.channel, {type: "info", msg: disconnectMsg.replace('%s',authToken.username)});
+    socket.on('subscribe', function (data) {
+      if (authToken){
+        scServer.global.publish(data, {type: "info", msg: connectMsg.replace('%s',authToken.username)});
+      }
     });
+
+    socket.on('unsubscribe', function (data) {
+      if (authToken){
+        scServer.global.publish(data, {type: "info", msg: disconnectMsg.replace('%s',authToken.username)});
+      }
+    });
+    
   });
 
 };
