f7814180e9a97efcec2d6af101a76ca335c8040f
Quan Yang <quanyang@live.com>
2016-05-19 02:38:50 +0200

    testing

--- socketcluster/griddit-chat/worker.js
+++ socketcluster/griddit-chat/worker.js
@@ -42,7 +42,8 @@ module.exports.run = function (worker) {
       //Validate user
       // Check data.username
       socket.setAuthToken({username: generateGuestId(), color: generateRandomColor()});
-      console.log("Authing" + socket.getAuthToken().username());
+      console.log("Authing" + socket.getAuthToken().username;
+      res();
     });
 
     socket.on('chat', function (data) {
