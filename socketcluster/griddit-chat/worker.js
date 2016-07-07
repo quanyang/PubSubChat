3d3e67df19d79319e22d8042afb6ab3f28f9f1bc
Quan Yang <quanyang@live.com>
2016-07-07 22:23:48 +0200

    removed MOTD

--- socketcluster/griddit-chat/worker.js
+++ socketcluster/griddit-chat/worker.js
@@ -9,7 +9,7 @@ var connectMsg = "Info: %s has joined the chatroom.";
 var disconnectMsg = "Info: %s has left the chatroom.";
 var welcomeMsg = "You are %s!";
 var registerMsg = "Register <a href='http://griddit.io/users/new'>here</a> for your own unique username!";
-var motd = "<div style='color:#08c'>Updates:</br>Message history now expires after 1 hour.</br>/who has been fixed, no more ghost user.</br>- Quanyang</div>";
+var motd = "";
 var userListMsg = "Users online: %s";
 var startMsgHistory = "Showing message history.";
 var endMsgHistory = "End of message history.";
