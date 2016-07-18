55c1c95d02e11be049adebe7b5f0b1e763b59de5
Quan Yang <quanyang@live.com>
2016-07-18 20:24:14 +0200

    Remove history logging

--- socketcluster/griddit-chat/worker.js
+++ socketcluster/griddit-chat/worker.js
@@ -164,7 +164,7 @@ module.exports.run = function (worker) {
           data.time = time.getTime();
           data.type = "message";
           data.isRegistered = authToken.isRegistered;
-          fs.appendFile('/root/log.txt', time.timeNow() + "#" + data.username + "@" + data.channel + ": " + data.msg + "\r\n", function (err) {});
+          //fs.appendFile('/root/log.txt', time.timeNow() + "#" + data.username + "@" + data.channel + ": " + data.msg + "\r\n", function (err) {});
           if (historyLength > 0) {
             if (data.channel in history) {
               history[data.channel].push(data);
