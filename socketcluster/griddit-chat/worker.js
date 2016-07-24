7207c38e21a4bfaad47e6fac832a67781cfd8707
Quan Yang <quanyang@live.com>
2016-07-24 19:01:04 +0200

    Make username lowercase when checking

--- socketcluster/griddit-chat/worker.js
+++ socketcluster/griddit-chat/worker.js
@@ -168,7 +168,7 @@ module.exports.run = function (worker) {
         var cmd = msg.split(" ")[0].toLowerCase();
         if (cmd in commands) {
           commands[cmd](scServer,socket,data);
-        } else if (cmd in adminCommands && authToken.username in admins) {
+        } else if (cmd in adminCommands && authToken.username.toLowerCase() in admins) {
           adminCommands[cmd](scServer,socket,data);
         } else {
           var time = new Date();
